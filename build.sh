da370126ade41684ad28b78515581ae35da21004
fjrXTR <fajarslebew31@gmail.com>
2024-04-21 10:43:51 +0000

    kernel: rebrand & import script for build
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (C) 2020-2021 Adithya R.
+
+SECONDS=0 # builtin bash timer
+ZIPNAME="villhaze!kernel-ruby-A13+-$(date '+%Y%m%d-%H%M').zip"
+TC_DIR="$HOME/.cos/xrage"
+AK3_DIR="$HOME/.cos/AnyKernel3"
+DEFCONFIG="fleur_defconfig"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+if ! [ -d "$TC_DIR" ]; then
+echo "clang not found! Cloning to $TC_DIR..."
+if ! git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang -b main $TC_DIR; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+export KBUILD_BUILD_USER=pajar
+export KBUILD_BUILD_HOST=workstation
+
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+cp out/defconfig arch/arm64/configs/$DEFCONFIG
+exit
+fi
+
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j7 O=out ARCH=arm64 CC=clang LD=ld.lld CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz
+
+if [ -f "out/arch/arm64/boot/Image.gz" ]; then
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -d "$AK3_DIR" ]; then
+cp -r $AK3_DIR AnyKernel3
+elif ! git clone -q https://github.com/in1tialford/AnyKernel3 -b ruby; then
+echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+exit 1
+fi
+cp out/arch/arm64/boot/Image.gz AnyKernel3
+rm -f *zip
+cd AnyKernel3
+git checkout master &> /dev/null
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+cd ..
+rm -rf AnyKernel3
+rm -rf out/arch/arm64/boot
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo "Zip: $ZIPNAME"
+if ! [[ $HOSTNAME = "ubuntu" && $USER = "nobody" ]]; then
+curl --upload-file $ZIPNAME http://oshi.at/$ZIPNAME; echo
+fi
+else
+echo -e "\nCompilation failed!"
+exit 1
+fi
